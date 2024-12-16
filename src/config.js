65a766e52d8231faa9a0054c1361f5f1a526f4c2
Satya Janghu <janghu.satya@gmail.com>
2024-12-17 04:56:22 +0530

    changed api to https

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: process.env.REACT_APP_API_URL || 'http://boostrank.me:5000',
+    API_BASE_URL: process.env.REACT_APP_API_URL || 'https://boostrank.me:5000',
 };
 
 export default config; 
